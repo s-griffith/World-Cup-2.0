1fc726d3d1c684a39646ec6b85e6035c3cd18121
Sarah <cmcgriffith@gmail.com>
2022-12-31 16:23:53 +0200

    fixed bash

--- test.sh
+++ test.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-TESTS_TO_RUN=10
+TESTS_TO_RUN=2
+EXECUTABLE=./FileTester
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 shopt -s nullglob
-FAILED_TESTS=0
+FAILED_TESTS=""
 
-
-for i in fileTests/inFiles/test*.in
+for i in inFiles/test*.in
 do
-	if [[ ${i//[^0-9]/} -gt $TESTS_TO_RUN ]]; then
+	if [ ${i//[^0-9]/} -gt $TESTS_TO_RUN ]; then
 		continue
 	fi
 	
 	printf "test $i >>>  "
-	./FileTester $i fileTests/inFiles/test${i//[^0-9]/}.in fileTests/outFiles/test${i//[^0-9]/}.result
-	diff fileTests/outFiles/test${i//[^0-9]/}.out fileTests/outFiles/test${i//[^0-9]/}.result
+	$EXECUTABLE < $i > outFiles/test${i//[^0-9]/}.result
+	diff outFiles/output${i//[^0-9]/}.out outFiles/test${i//[^0-9]/}.result
 
 	if [ $? -eq 0 ]
 	then
-		printf "Game Simulation: ${GREEN}pass${NC},   "
+		printf "Test Run: ${GREEN}pass${NC},   "
 	else
-		printf "Game Simulation: ${RED}fail${NC},   "
-		FAILED_TESTS+=1
+		printf "Test Run: ${RED}fail${NC},   "
+		FAILED_TESTS+='-'
+		FAILED_TESTS+='F'
 	fi
-	valgrind --log-file=$i.valgrind_log --leak-check=full ./FileTester $i fileTests/inFiles/test${i//[^0-9]/}.in fileTests/outFiles/test${i//[^0-9]/}.vresult 1>/dev/null 2>/dev/null
-	rm  fileTests/outFiles/test${i//[^0-9]/}.vresult
+	valgrind --log-file=$i.valgrind_log --leak-check=full $EXECUTABLE < $i 1>/dev/null 2>/dev/null
 	if [ -f $i.valgrind_log ]
 	then
 		cat $i.valgrind_log | grep "ERROR SUMMARY: 0" > /dev/null
@@ -38,18 +38,18 @@ do
 		else
 			printf "Leak: ${RED}fail${NC}\n"
 			cat $i.valgrind_log
-			FAILED_TESTS+=1
+			FAILED_TESTS+="-"
 		fi
 	else
-		printf "Leak: ${RED}couldn't get valgrind file${NC}\n"
-		FAILED_TESTS+=1
+		printf "Leak: ${RED}couldnt get valgrind file${NC}\n"
+		FAILED_TESTS+="-"
 	fi
 	rm $i.valgrind_log
 done
 
-if [ ${FAILED_TESTS} -eq 0 ]; then
+if [ -z ${FAILED_TESTS} ]; then
 	printf "\n${GREEN} All tests passed :)${NC}\n\n"
 else
 	printf "\n${RED} Failed ${FAILED_TESTS}${NC} tests.\n\n"
 fi
-printf "incress TESTS_TO_RUN in tester.sh in order to run more tests (up to 4030)\n"
+printf "incress TESTS_TO_RUN in tester.sh in order to run more tests (up to 500)\n"
